1e5cbf084e01fc4f38aa636b0b4e60c05020081b
Khaled S Momani <khaledmomani4@gmail.com>
2021-06-30 03:30:08 +0000

    Automatically backed up by Learn

--- lib/contacts.rb
+++ lib/contacts.rb
@@ -17,22 +17,10 @@ require 'pry'
 def remove_strawberry(contacts)
   contacts.each do |person, contact_details_hash|
     if person == "Freddy Mercury"
-      contact_details_has.each do |atribute, data|
+      contact_details_hash.each do |atribute, data|
         data.delete_if {ice_cream| ice_cream == "strawberry"}
       end
     end
   end
 end
-end
-
-def remove_strawberry(contacts)
-  contacts.each do |person, contact_details_hash|
-    if person == "Freddy Mercury"
-      contact_details_hash.each do |attribute, data|
-        if attribute == :favorite_ice_cream_flavors
-          data.delete_if {|ice_cream| ice_cream == "strawberry"}
-        end
-      end
-    end
-  end
 end
